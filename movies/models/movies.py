63edfe3ed57ef9f6b4fcb4e18e44e02545bf7d13
Luka <lukagoderdzishvili4@gmail.com>
2024-01-03 17:49:50 +0400

    [Luka] Added fields and form/tree views

--- movies/models/movies.py
+++ movies/models/movies.py
@@ -1,6 +1,16 @@
 from odoo import fields, models
 
 
+
 class MoviesMovies(models.Model):
     _name = "movies.movies"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Movies"
+
+    name = fields.Char(string="Title")
+    release_date = fields.Date()
+    duration = fields.Float()
+    description = fields.Text()
+    language = fields.Selection([("geo", "Geo"),("eng", "Eng")])
+    image = fields.Image(string='Poster', max_width=590, max_height=590)
+    rating = fields.Float()
